46955aed6e09d85bfc1bd459c56415bea5ab37b9
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2018-11-11 10:41:30 -0500

    how about this

--- set_js_url.sh
+++ set_js_url.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 WORKING_DIRECTORY=`dirname $0`
-echo $WORKING_DIRECTORY
 
 cd ${WORKING_DIRECTORY}
-
+echo $(pwd)
 CUBENAME=$(echo "$CLOUDCUBE_URL" | awk -F "/" ' { print $4 }')
 
 sed "s/CUBENAME/${CUBENAME}/g" index-prod.html > static/index.html
