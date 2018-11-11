cf1a31aca9695b5aa636c9007183afa8415cdc8c
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2018-11-11 09:22:32 -0500

    Try this

--- set_js_url.sh
+++ set_js_url.sh
@@ -2,6 +2,9 @@
 
 CUBENAME=$(echo "$CLOUDCUBE_URL" | awk -F "/" ' { print $4 }')
 
+rm -f static/index.html
+touch static/index.html
+
 sed "s/CUBENAME/${CUBENAME}/g" index-prod.html > static/index.html
 
 
