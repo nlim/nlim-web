c5930d016e80d31b6e06afcd83de0d5127dfb391
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2018-11-11 08:51:17 -0500

    fix script

--- set_js_url.sh
+++ set_js_url.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-HEROKU_CONFIG=$(heroku config -a nlim)
-
-CUBENAME=$(echo "$HEROKU_CONFIG" | grep  "CLOUDCUBE_URL" | awk '{ print $2 }' | awk -F "/" ' { print $4 }')
+CUBENAME=$(echo "$CLOUDCUBE_URL" | awk -F "/" ' { print $4 }')
 
 sed "s/CUBENAME/${CUBENAME}/g" static/index-prod.html > static/index.html
 
